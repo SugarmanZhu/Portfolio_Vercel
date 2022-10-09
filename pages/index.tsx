012da35127f4624af6f24dd3b32788d10ab10cc4
Xiaotan Zhu <sugarmanzhu@gmail.com>
2022-10-09 11:01:51 +0100

    skip hero part to compile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
-import Hero from '../components/Hero'
+// import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
@@ -12,9 +12,9 @@ const Home: NextPage = () => {
       
       <Header />
 
-      <section id='hero' className='snap-center'>
+      {/* <section id='hero' className='snap-center'>
         <Hero />
-      </section>      
+      </section>       */}
     </div>
   )
 }
