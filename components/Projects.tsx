e3ee3bc3d418961b2c1db431a4bd437639bb02a2
Xiaotan Zhu <sugarmanzhu@gmail.com>
2022-10-09 23:38:27 +0100

    Add proeject key to compile

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -17,7 +17,7 @@ function Projects({}: Props) {
 
       <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
         {projects.map((project, index) => (
-          <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+          <div key={index} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
             <motion.img 
               initial={{ y: -50, opacity: 0 }}
               whileInView={{ y: 0, opacity: 1 }}
