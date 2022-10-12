61100d56aeba443adfe854a1b83eaf9869550e09
Xiaotan Zhu <sugarmanzhu@gmail.com>
2022-10-12 10:20:05 +0100

    adjust skill text style

--- components/SkillElement.tsx
+++ components/SkillElement.tsx
@@ -24,7 +24,7 @@ function SkillElement({ skill, directionLeft }: Props) {
         />
         <div className='absolute opacity-0 group-hover:opacity-80 trnasition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:h-28 md:w-28 xl:h-32 xl:w-32 rounded-full z-0'>
             <div className='flex items-center justify-center h-full'>
-                <p className='text-3xl font-bold text-black opacity-100'>{skill.title}</p>
+                <p className='text-2xl font-bold text-black opacity-100 p-5'>{skill.title}</p>
             </div>
         </div>
     </div>
