c5e01eca705217e2fb8c49d1725075b66f75d379
Xiaotan Zhu <sugarmanzhu@gmail.com>
2022-10-12 03:15:26 +0100

    add project build repo links

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -40,6 +40,20 @@ function Projects({ projects }: Props) {
                 </span> {project.title}
               </h4>
 
+              <div className='flex items-center justify-center space-x-3'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+                </svg>
+                <a href={`${project.linkToBuild}`}> Project Build Link </a>
+              </div>
+
+              <div className='flex items-center justify-center space-x-3'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+                </svg>
+                <a href={`${project.linkToRepo}`}> Project Repo Link </a>
+              </div>
+
               <div className='flex items-center space-x-2 justify-center'>
                 {project.technologies.map((technology) => (
                   <img 
