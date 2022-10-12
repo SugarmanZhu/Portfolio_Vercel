4566aa45ab6cc4da2dc971740dd1e89e00d0425b
Xiaotan Zhu <sugarmanzhu@gmail.com>
2022-10-12 16:51:37 +0100

    Update date format

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -8,6 +8,26 @@ type Props = {
 }
 
 function ExperienceCard({ experience }: Props) {
+    const formatDate = (date: string) => {
+        const months = [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ];
+        const d = new Date(date);
+        const month = d.getMonth();
+        const year = d.getFullYear();
+        return `${months[month]} ${year}`;
+      };
   return (
     <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w=[900px] snap-center md:bg-[#292929] p-10 hover:opacity-100 md:opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
         <motion.img 
@@ -32,8 +52,8 @@ function ExperienceCard({ experience }: Props) {
                     />
                 ))}
             </div>
-            <p className='uppercase py-5 text-gray-300'>
-                {new Date(experience.dateStarted).toDateString()} - {experience.isCurrentlyWorkingHere ? "Present" : new Date(experience.dateEnded).toDateString()}
+            <p className='py-5 text-gray-300'>
+                {formatDate(experience.dateStarted)} - {experience.isCurrentlyWorkingHere ? "Present" : formatDate(experience.dateEnded)}
             </p>
             <ul className='list-disc space-y-4 ml-5 text-lg h-96'>
                 {experience.points.map((point, i) => (
